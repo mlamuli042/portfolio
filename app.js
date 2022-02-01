77cc15c3cfe958b607e5596a4b4b85adc635bd04
mlamuli042 <sonkemasilela@gmail.com>
2022-02-01 15:50:48 +0200

    last commit

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require('express');
-
+const exphbs = require('express-handlebars');
+const path = require('path');
 const app = express();
+
+const { formatDate } = require('./helpers/dateFormat');
+app.engine('handlebars', exphbs({
+   helpers: { formatDate: formatDate },
+   defaultLayout: 'main'
+  }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/fa', express.static(__dirname + '/node_modules/font-awesome/css'));
+app.use('/fonts', express.static(__dirname + '/node_modules/font-awesome/fonts'));
+
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(__dirname + '/node_modules/@popperjs/core/dist/umd'));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
 
 const port = process.env.PORT || 3000;
 
